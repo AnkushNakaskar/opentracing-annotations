0dc4e9bb04c50f3de9a35781f7924d32f0bf073a
Ankush Nakaskar <ankush.nakaskar@phonepe.com>
2025-02-27 11:47:11 +0530

    Added test with tracer and span with correct tags

--- src/test/java/io/appform/opentracing/TracingHandlerTest.java
+++ src/test/java/io/appform/opentracing/TracingHandlerTest.java
@@ -27,15 +27,28 @@ import java.util.Map;
  */
 class TracingHandlerTest {
 
+    private TagCapturingHandler tagCapturingHandler;
+    private Tracer tracer;
+
+    private static final Map<String,String> localTagMap = new HashMap<>();
+
 
     @BeforeEach
     void setup() {
-
+        localTagMap.clear();
+        tagCapturingHandler = new TagCapturingHandler();
+        Tracing tracing = Tracing.newBuilder()
+                .sampler(Sampler.ALWAYS_SAMPLE)
+                .addSpanHandler(tagCapturingHandler) // Register the custom handler
+                .build();
+         tracer = TracerUtil.getTracer(tracing);
 
     }
 
     @AfterEach
     void cleanup() {
+        tracer.close();
+        localTagMap.clear();
     }
 
     @Test
@@ -47,12 +60,6 @@ class TracingHandlerTest {
     @Test
     void testStartSpan() {
 
-        TagCapturingHandler tagCapturingHandler = new TagCapturingHandler();
-        Tracing tracing = Tracing.newBuilder()
-                .sampler(Sampler.ALWAYS_SAMPLE)
-                .addSpanHandler(tagCapturingHandler) // Register the custom handler
-                .build();
-        Tracer tracer = TracerUtil.getTracer(tracing);
         final String methodName = "test";
         final String className = "testClass";
 
@@ -62,19 +69,13 @@ class TracingHandlerTest {
         Assertions.assertNotNull(span);
         Assertions.assertTrue(span instanceof BraveSpan);
         System.out.println(tagCapturingHandler.getTags());
-        Assertions.assertEquals(methodName, tagCapturingHandler.getTags().get(TracingConstants.METHOD_NAME_TAG));
-        Assertions.assertEquals(className, tagCapturingHandler.getTags().get(TracingConstants.CLASS_NAME_TAG));
-        Assertions.assertEquals("test", tagCapturingHandler.getTags().get(TracingConstants.PARAMETER_STRING_TAG));
+        Assertions.assertEquals(methodName, localTagMap.get(TracingConstants.METHOD_NAME_TAG));
+        Assertions.assertEquals(className, localTagMap.get(TracingConstants.CLASS_NAME_TAG));
+        Assertions.assertEquals("test", localTagMap.get(TracingConstants.PARAMETER_STRING_TAG));
     }
 
     @Test
     void testStartScope() {
-        TagCapturingHandler tagCapturingHandler = new TagCapturingHandler();
-        Tracing tracing = Tracing.newBuilder()
-                .sampler(Sampler.ALWAYS_SAMPLE)
-                .addSpanHandler(tagCapturingHandler) // Register the custom handler
-                .build();
-        Tracer tracer = TracerUtil.getTracer(tracing);
         Assertions.assertNull(TracingHandler.startScope(null, NoopSpan.INSTANCE));
         Assertions.assertNull(TracingHandler.startScope(GlobalTracer.get(), null));
         final String methodName = "test";
@@ -84,17 +85,11 @@ class TracingHandlerTest {
         Assertions.assertNotNull(scope);
         Assertions.assertTrue(scope instanceof BraveScope);
         span.finish();
-
+        tracer.close();
     }
 
     @Test
     void testAddSuccessTagToSpan() {
-        TagCapturingHandler tagCapturingHandler = new TagCapturingHandler();
-        Tracing tracing = Tracing.newBuilder()
-                .sampler(Sampler.ALWAYS_SAMPLE)
-                .addSpanHandler(tagCapturingHandler) // Register the custom handler
-                .build();
-        Tracer tracer = TracerUtil.getTracer(tracing);
         final String methodName = "test";
         final String className = "testClass";
         Span span = TracingHandler.startSpan(new FunctionData(className, methodName), "test");
@@ -102,17 +97,11 @@ class TracingHandlerTest {
         Assertions.assertDoesNotThrow(() -> TracingHandler.addSuccessTagToSpan(null));
         Assertions.assertDoesNotThrow(() -> TracingHandler.addSuccessTagToSpan(tracer.activeSpan()));
         span.finish();
-        Assertions.assertEquals("SUCCESS", tagCapturingHandler.getTags().get(TracingConstants.METHOD_STATUS_TAG));
+        Assertions.assertEquals("SUCCESS", localTagMap.get(TracingConstants.METHOD_STATUS_TAG));
     }
 
     @Test
     void testAddErrorTagToSpan() {
-        TagCapturingHandler tagCapturingHandler = new TagCapturingHandler();
-        Tracing tracing = Tracing.newBuilder()
-                .sampler(Sampler.ALWAYS_SAMPLE)
-                .addSpanHandler(tagCapturingHandler) // Register the custom handler
-                .build();
-        Tracer tracer = TracerUtil.getTracer(tracing);
         final String methodName = "test";
         final String className = "testClass";
         Span span = TracingHandler.startSpan(new FunctionData(className, methodName), "test");
@@ -122,17 +111,11 @@ class TracingHandlerTest {
         span.finish();
         tracer.close();
         tagCapturingHandler.getTags().get(TracingConstants.METHOD_STATUS_TAG);
-        Assertions.assertEquals("FAILURE", tagCapturingHandler.getTags().get(TracingConstants.METHOD_STATUS_TAG));
+        Assertions.assertEquals("FAILURE", localTagMap.get(TracingConstants.METHOD_STATUS_TAG));
     }
 
     @Test
     void testCloseSpanAndScope() {
-        TagCapturingHandler tagCapturingHandler = new TagCapturingHandler();
-        Tracing tracing = Tracing.newBuilder()
-                .sampler(Sampler.ALWAYS_SAMPLE)
-                .addSpanHandler(tagCapturingHandler) // Register the custom handler
-                .build();
-        Tracer tracer = TracerUtil.getTracer(tracing);
         Assertions.assertDoesNotThrow(() -> TracingHandler.closeSpanAndScope(null, null));
         Assertions.assertDoesNotThrow(() -> TracingHandler.closeSpanAndScope(null, NoopScopeManager.NoopScope.INSTANCE));
         Assertions.assertDoesNotThrow(() -> TracingHandler.closeSpanAndScope(NoopSpan.INSTANCE, null));
@@ -141,12 +124,11 @@ class TracingHandlerTest {
 
     static class TagCapturingHandler extends SpanHandler {
 
-        private Map<String,String> localTagMap = new HashMap<>();
         @Override
         public boolean end(TraceContext context, MutableSpan span, Cause cause) {
             Map<String, String> tags = span.tags();
             System.out.println("Captured Tags: " + tags);
-            this.localTagMap.putAll(tags);
+            localTagMap.putAll(tags);
             return true;
         }
         public Map<String,String> getTags(){
